1252b8b4815c86e3823aceca3df3c1f855b1dc31
Victor, Jason M <jason_victor@merck.com>
2014-05-30 09:13:14 -0400

    Initial Commit

---
+++ testing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testing
+//
+//  Created by Victor, Jason M on 5/30/14.
+//  Copyright (c) 2014 Victor, Jason M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testing
+//
+//  Created by Victor, Jason M on 5/30/14.
+//  Copyright (c) 2014 Victor, Jason M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
